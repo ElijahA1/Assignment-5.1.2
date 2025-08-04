e4235fb3484b5a301f2d2155217d105463a71488
ElijahA1 <elijah.abourezk@outlook.com>
2025-08-04 12:42:03 -0700

    Add project files.

---
+++ Assignment 5.1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment_5._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1234;
+
+            Console.WriteLine(sumOfDigits(number));
+        }
+        static int sumOfDigits(int number)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                // Get the next integer or digit in the number
+                int numberElement = number % 10;
+
+                // Remove that integer or digit from number and add it to the sum
+                number /= 10;
+
+                sum += numberElement;
+            }
+            return sum;
+        }
+    }
+}
